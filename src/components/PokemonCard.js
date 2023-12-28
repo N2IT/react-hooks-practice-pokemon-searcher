4c1c61532d8b05a7703c4d754593f58e1ad53864
Tony Eder <tonyeder11@gmail.com>
2023-12-28 12:06:55 -0800

    quasi rendering. need to iterate through image links

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+  const { id, name, hp, sprites } = pokemon
+  // console.log(pokemon)
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div key={id} className="image">
+          <img alt={name} />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
