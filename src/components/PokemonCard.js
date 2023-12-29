b63eb8fae6ea1a78917eda2d9ab393d6d0c806c9
Tony Eder <tonyeder11@gmail.com>
2023-12-29 09:46:14 -0800

    image swap complete

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({ pokemon }) {
   const { id, name, hp, sprites } = pokemon
   // console.log(sprites.front)
-  // const [ spriteFront, setFrontSprite ] = useState()
+  const [ isSpriteFront, setFrontSprite ] = useState(true)
+
+  function handleImgSwap() {
+    setFrontSprite(!isSpriteFront)
+  }
   
   return (
     <Card>
       <div>
         <div key={id} className="image">
-          {<img src={sprites.front} alt={name} />}
+          {<img onClick={handleImgSwap} src={isSpriteFront ? sprites.front : sprites.back} alt={name} />}
         </div>
         <div className="content">
           <div className="header">{name}</div>
